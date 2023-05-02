dd071004bc70f1da45904a2a88a12331bdc616dc
nisba <87497052+unionx09@users.noreply.github.com>
2023-05-02 16:43:40 +0700

    Add files via upload

---
+++ KeyAuth-x86-nisba/main.cpp
@@ -0,0 +1,183 @@
+#include <Windows.h>
+#include "auth.hpp"
+#include <string>
+#include "skStr.h"
+std::string tm_to_readable_time(tm ctx);
+static std::time_t string_to_timet(std::string timestamp);
+static std::tm timet_to_tm(time_t timestamp);
+
+using namespace KeyAuth;
+
+std::string name = "kfleko565"; // application name. right above the blurred text aka the secret on the licenses tab among other tabs
+std::string ownerid = "V3774KxKUg"; // ownerid, found in account settings. click your profile picture on top right of dashboard and then account settings.
+std::string secret = "9b35255b7c061e541e09b3cd1a24b592c07e5fef9defe1e3382f897f2fa4046b"; // app secret, the blurred text on licenses tab and other tabs
+std::string version = "1.0"; // leave alone unless you've changed version on website
+std::string url = "https://keyauth.win/api/1.1/"; // change if you're self-hosting
+std::string sslPin = "ssl pin key (optional)"; // don't change unless you intend to pin public certificate key. you can get here in the "Pin SHA256" field https://www.ssllabs.com/ssltest/analyze.html?d=keyauth.win&latest. If you do this you need to be aware of when SSL key expires so you can update it
+
+/*
+	Video on what ownerid and secret are https://youtu.be/uJ0Umy_C6Fg
+
+	Video on how to add KeyAuth to your own application https://youtu.be/GB4XW_TsHqA
+
+	Video to use Web Loader (control loader from customer panel) https://youtu.be/9-qgmsUUCK4
+*/
+
+api KeyAuthApp(name, ownerid, secret, version, url, sslPin);
+
+int main()
+{
+	SetConsoleTitleA(skCrypt("Loader"));
+	std::cout << skCrypt("\n\n Connecting..");
+	KeyAuthApp.init();
+	if (!KeyAuthApp.data.success)
+	{
+		std::cout << skCrypt("\n Status: ") << KeyAuthApp.data.message;
+		Sleep(1500);
+		exit(0);
+	}
+
+	std::cout << skCrypt("\n\n App data:");
+	std::cout << skCrypt("\n Number of users: ") << KeyAuthApp.data.numUsers;
+	std::cout << skCrypt("\n Number of online users: ") << KeyAuthApp.data.numOnlineUsers;
+	std::cout << skCrypt("\n Number of keys: ") << KeyAuthApp.data.numKeys;
+	std::cout << skCrypt("\n Application Version: ") << KeyAuthApp.data.version;
+	std::cout << skCrypt("\n Customer panel link: ") << KeyAuthApp.data.customerPanelLink;
+	std::cout << skCrypt("\n Checking session validation status (remove this if causing your loader to be slow)");
+	KeyAuthApp.check();
+	std::cout << skCrypt("\n Current Session Validation Status: ") << KeyAuthApp.data.message;
+
+	std::cout << skCrypt("\n\n [1] Login\n [2] Register\n [3] Upgrade\n [4] License key only\n\n Choose option: ");
+
+	int option;
+	std::string username;
+	std::string password;
+	std::string key;
+
+	std::cin >> option;
+	switch (option)
+	{
+	case 1:
+		std::cout << skCrypt("\n\n Enter username: ");
+		std::cin >> username;
+		std::cout << skCrypt("\n Enter password: ");
+		std::cin >> password;
+		KeyAuthApp.login(username, password);
+		break;
+	case 2:
+		std::cout << skCrypt("\n\n Enter username: ");
+		std::cin >> username;
+		std::cout << skCrypt("\n Enter password: ");
+		std::cin >> password;
+		std::cout << skCrypt("\n Enter license: ");
+		std::cin >> key;
+		KeyAuthApp.regstr(username, password, key);
+		break;
+	case 3:
+		std::cout << skCrypt("\n\n Enter username: ");
+		std::cin >> username;
+		std::cout << skCrypt("\n Enter license: ");
+		std::cin >> key;
+		KeyAuthApp.upgrade(username, key);
+		break;
+	case 4:
+		std::cout << skCrypt("\n Enter license: ");
+		std::cin >> key;
+		KeyAuthApp.license(key);
+		break;
+	default:
+		std::cout << skCrypt("\n\n Status: Failure: Invalid Selection");
+		Sleep(3000);
+		exit(0);
+	}
+
+	if (!KeyAuthApp.data.success)
+	{
+		std::cout << skCrypt("\n Status: ") << KeyAuthApp.data.message;
+		Sleep(1500);
+		exit(0);
+	}
+
+	std::cout << skCrypt("\n User data:");
+	std::cout << skCrypt("\n Username: ") << KeyAuthApp.data.username;
+	std::cout << skCrypt("\n IP address: ") << KeyAuthApp.data.ip;
+	std::cout << skCrypt("\n Hardware-Id: ") << KeyAuthApp.data.hwid;
+	std::cout << skCrypt("\n Create date: ") << tm_to_readable_time(timet_to_tm(string_to_timet(KeyAuthApp.data.createdate)));
+	std::cout << skCrypt("\n Last login: ") << tm_to_readable_time(timet_to_tm(string_to_timet(KeyAuthApp.data.lastlogin)));
+	std::cout << skCrypt("\n Subscription name(s): ");
+	std::string subs;
+	for (std::string value : KeyAuthApp.data.subscriptions)subs += value + " ";
+	std::cout << subs;
+	std::cout << skCrypt("\n Subscription expiry: ") << tm_to_readable_time(timet_to_tm(string_to_timet(KeyAuthApp.data.expiry)));
+	std::cout << skCrypt("\n Checking session validation status (remove this if causing your loader to be slow)");
+	KeyAuthApp.check();
+	std::cout << skCrypt("\n Current Session Validation Status: ") << KeyAuthApp.data.message;
+	
+	/*
+	KeyAuthApp.web_login();
+
+	std::cout << "\n Waiting for button to be clicked";
+	KeyAuthApp.button("close");
+	*/
+
+	/*
+	for (std::string subs : KeyAuthApp.data.subscriptions)
+	{
+		if (subs == "default")
+		{
+			std::cout << skCrypt("\n User has subscription with name: default");
+		}
+	}
+	*/
+
+	/*
+	// download file, change file.exe to whatever you want.
+	// remember, certain paths like windows folder will require you to turn on auto run as admin https://stackoverflow.com/a/19617989
+
+	std::vector<std::uint8_t> bytes = KeyAuthApp.download("167212");
+	std::ofstream file("file.exe", std::ios_base::out | std::ios_base::binary);
+	file.write((char*)bytes.data(), bytes.size());
+	file.close();
+	*/
+
+	// KeyAuthApp.setvar("discord", "test#0001"); // set the variable 'discord' to 'test#0001'
+	// std::cout << "\n\n User variable data: " + KeyAuthApp.getvar("discord"); // display the user variable witn name 'discord'
+
+	// let's say you want to send request to https://keyauth.win/api/seller/?sellerkey=f43795eb89d6060b74cdfc56978155ef&type=black&ip=1.1.1.1&hwid=abc
+	// but doing that from inside the loader is a bad idea as the link could get leaked.
+	// Instead, you should create a webhook with the https://keyauth.win/api/seller/?sellerkey=f43795eb89d6060b74cdfc56978155ef part as the URL
+	// then in your loader, put the rest of the link (the other paramaters) in your loader. And then it will send request from KeyAuth server and return response in string resp
+
+	// you have to encode the & sign with %26
+	// std::string resp = KeyAuthApp.webhook("P5NHesuZyf", "%26type=black%26ip=1.1.1.1%26hwid=abc");
+	// std::cout << "\n Response recieved from webhook request: " + resp;
+
+	// KeyAuthApp.log("user logged in"); // send event to logs. if you set discord webhook in app settings, it will send there too
+	// KeyAuthApp.ban(); // ban the current user, must be logged in
+
+	std::cout << skCrypt("\n\n Closing in ten seconds...");
+	Sleep(10000);
+	exit(0);
+}
+
+std::string tm_to_readable_time(tm ctx) {
+	char buffer[80];
+
+	strftime(buffer, sizeof(buffer), "%a %m/%d/%y %H:%M:%S %Z", &ctx);
+
+	return std::string(buffer);
+}
+
+static std::time_t string_to_timet(std::string timestamp) {
+	auto cv = strtol(timestamp.c_str(), NULL, 10); // long
+
+	return (time_t)cv;
+}
+
+static std::tm timet_to_tm(time_t timestamp) {
+	std::tm context;
+
+	localtime_s(&context, &timestamp);
+
+	return context;
+}
